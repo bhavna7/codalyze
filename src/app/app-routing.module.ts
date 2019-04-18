02b35d7b33a3d62fb37d2cff7bdb1969b9f5663f
bhavna jain <bhavnajain_16@hotmail.com>
2019-04-18 13:21:59 +0530

    enhanced routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { EditProductDetailsComponent } from './edit-product-details/edit-product-details.component';
 
 export const routes: Routes = [
-  { path: '', component: ProductDetailsComponent, pathMatch: 'full'},
+  { path: '', component: ProductDetailsComponent},
   { path: 'editProduct', component: EditProductDetailsComponent }
 ];
 
